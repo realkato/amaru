65819bb8d26115ab7fd064f181dfc00375f6207c
realkato <68985723+realkato@users.noreply.github.com>
2020-07-31 14:54:43 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I love HaSeoyoon02');
+    res.send('I love HaSeoyoon03');
 });
 
 app.get('/start', function(req, res){
