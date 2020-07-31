75ff1d9a5d638654707c81af53253ed0f65b4479
balmung1 <balmung1@naver.com>
2020-07-31 14:29:54 +0900

    2nd commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I love Hoichang');
+    res.send('I love HaSeoyoon');
 });
 
 app.get('/start', function(req, res){
